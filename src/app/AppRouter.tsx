cfd925a999243af066c1ae5bc83ccd5b254e5912
petaurora <325150851+petaurora@users.noreply.github.com>
2026-09-10 22:41:37 -0500

    M18.3: route quiz and results pages

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProfileAppRoot from "../ProfileAppRoot";
 import { HubPage } from "../features/hub/HubPage";
+import { QuizRoutePage } from "../features/quizzes/QuizRoutePage";
 import { AppShell } from "./AppShell";
 import { SettingsRoute } from "./SettingsRoute";
 import {
   hubRoute,
   legacyScreenRoutes,
-  reservedRoutes,
+  quizResultsRoute,
+  quizRoute,
   settingsRoute,
 } from "./routes";
 
@@ -16,20 +18,20 @@ export function RoutedApplication() {
       <Route element={<AppShell />}>
         <Route path={hubRoute.path} element={<HubPage />} />
         <Route path={settingsRoute.path} element={<SettingsRoute />} />
+        <Route
+          path={quizRoute.path}
+          element={<QuizRoutePage mode="quiz" />}
+        />
+        <Route
+          path={quizResultsRoute.path}
+          element={<QuizRoutePage mode="results" />}
+        />
 
         {legacyScreenRoutes.map((route) => (
           <Route
             key={route.id}
             path={route.path}
             element={<ProfileAppRoot initialScreen={route.screen} />}
-          />
-        ))}
-
-        {reservedRoutes.map((route) => (
-          <Route
-            key={route.id}
-            path={route.path}
-            element={<Navigate to="/" replace />}
           />
         ))}
 
