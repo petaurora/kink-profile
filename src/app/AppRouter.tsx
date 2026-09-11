0fef2dc739626d7884efd84ff923200a615776cd
petaurora <325150851+petaurora@users.noreply.github.com>
2026-09-10 22:30:19 -0500

    M18.2 route Hub and Settings explicitly

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,12 +1,22 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProfileAppRoot from "../ProfileAppRoot";
+import { HubPage } from "../features/hub/HubPage";
 import { AppShell } from "./AppShell";
-import { legacyScreenRoutes, reservedRoutes } from "./routes";
+import { SettingsRoute } from "./SettingsRoute";
+import {
+  hubRoute,
+  legacyScreenRoutes,
+  reservedRoutes,
+  settingsRoute,
+} from "./routes";
 
 export function RoutedApplication() {
   return (
     <Routes>
       <Route element={<AppShell />}>
+        <Route path={hubRoute.path} element={<HubPage />} />
+        <Route path={settingsRoute.path} element={<SettingsRoute />} />
+
         {legacyScreenRoutes.map((route) => (
           <Route
             key={route.id}
