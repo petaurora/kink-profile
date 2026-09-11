7a625cc854ea8e84a8ef996b49171c277059cd21
petaurora <325150851+petaurora@users.noreply.github.com>
2026-09-11 16:17:23 -0500

    Test direct-route profile hydration from restored storage

--- src/app/currentProfileSnapshot.ts
+++ src/app/currentProfileSnapshot.ts
@@ -1,11 +1,11 @@
 import { loadCatalogProfile } from "../lib/catalogProfileStorage";
 import { buildCatalogResultView } from "../lib/catalogResults";
 import { buildCanonicalSignalProfile } from "../lib/overallProfileSignals";
-import { loadProfile } from "../lib/profileStorage";
+import { loadProfile, type StorageLike } from "../lib/profileStorage";
 
-export function loadCurrentProfileSnapshot() {
-  const profile = loadProfile();
-  const catalogProfile = loadCatalogProfile();
+export function loadCurrentProfileSnapshot(storage?: StorageLike) {
+  const profile = loadProfile(storage);
+  const catalogProfile = loadCatalogProfile(storage);
 
   return {
     profile,
