ab48ec05b157195c44d578b7c0a4aae0e8e65fb5
petaurora <325150851+petaurora@users.noreply.github.com>
2026-09-11 20:38:18 -0500

    M19.3: route Catalog workspaces and legacy redirects

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,4 +1,10 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { CatalogRoute } from "../features/catalog/CatalogRoute";
 import { CompareRoute } from "../features/comparison/CompareRoute";
 import { CurationRoute } from "../features/curation/CurationRoute";
@@ -6,16 +12,20 @@ import { HubPage } from "../features/hub/HubPage";
 import { ProfileRoute } from "../features/profile/ProfileRoute";
 import { QuizHomePage } from "../features/quizzes/QuizHomePage";
 import { QuizRoutePage } from "../features/quizzes/QuizRoutePage";
-import { RankingRoute } from "../features/ranking/RankingRoute";
+import { RewardsCatalogRoute } from "../features/rewards/RewardsCatalogRoute";
 import { RewardsRoute } from "../features/rewards/RewardsRoute";
 import { SceneBuilderRoute } from "../features/scenes/SceneBuilderRoute";
 import { SettingsRoute } from "../features/settings/SettingsRoute";
 import { AppShell } from "./AppShell";
 import {
+  catalogRewardsRankingRoute,
+  catalogRewardsRoute,
   catalogRoute,
   compareRoute,
   curationRoute,
   hubRoute,
+  legacyCatalogRoute,
+  legacyRankingRoute,
   profileRoute,
   quizHomeRoute,
   quizResultsRoute,
@@ -27,6 +37,11 @@ import {
   unknownRouteFallbackPath,
 } from "./routes";
 
+function LegacyCatalogRedirect() {
+  const location = useLocation();
+  return <Navigate to={`${catalogRoute.path}${location.search}`} replace />;
+}
+
 export function RoutedApplication() {
   return (
     <Routes>
@@ -34,9 +49,25 @@ export function RoutedApplication() {
         <Route path={hubRoute.path} element={<HubPage />} />
         <Route path={profileRoute.path} element={<ProfileRoute />} />
         <Route path={quizHomeRoute.path} element={<QuizHomePage />} />
-        <Route path={catalogRoute.path} element={<CatalogRoute />} />
-        <Route path={rankingRoute.path} element={<RankingRoute />} />
+
+        <Route path={catalogRoute.path} element={<CatalogRoute view="browse" />} />
+        <Route path={rankingRoute.path} element={<CatalogRoute view="rank" />} />
+        <Route
+          path={catalogRewardsRoute.path}
+          element={<RewardsCatalogRoute view="browse" />}
+        />
+        <Route
+          path={catalogRewardsRankingRoute.path}
+          element={<RewardsCatalogRoute view="rank" />}
+        />
+
+        <Route path={legacyCatalogRoute.path} element={<LegacyCatalogRedirect />} />
+        <Route
+          path={legacyRankingRoute.path}
+          element={<Navigate to={rankingRoute.path} replace />}
+        />
         <Route path={rewardsRoute.path} element={<RewardsRoute />} />
+
         <Route path={sceneBuilderRoute.path} element={<SceneBuilderRoute />} />
         <Route path={compareRoute.path} element={<CompareRoute />} />
         <Route path={curationRoute.path} element={<CurationRoute />} />
